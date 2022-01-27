44e4c00875af11d98e3f4d159038342de571b111
Maryana <ilon.mars@yandex.ru>
2022-01-27 17:38:56 +0300

    add: header

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,43 @@
 <template>
   <header class="header">
-    <nuxt-link class="header__cart-link" to="/cart">
-      <img src="~/assets/img/cart-icon.png" alt="">
+    <nuxt-link class="header__cart-link" to="/cart" data-counter="10">
+      <svg class="header__cart-icon">
+        <use xlink:href="~/assets/img/sprite.svg#cart"></use>
+      </svg>
     </nuxt-link>
   </header>
 </template>
+
+<style lang="sass">
+.header
+  padding: 10px
+  background-color: $main-color
+  display: flex
+  justify-content: flex-end
+
+  &__cart-link
+    display: flex
+    width: 40px
+    height: 40px
+    position: relative
+
+    &::before
+      content: attr(data-counter)
+      width: 18px
+      height: 18px
+      border-radius: 50%
+      position: absolute
+      top: 0
+      right: 0
+      background-color: $warning-color
+      font-size: 12px
+      display: flex
+      align-items: center
+      justify-content: center
+      color: $light-color
+
+  &__cart-icon
+    width: 100%
+    height: 100%
+    fill: $light-color
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -6,27 +6,22 @@ export default {
   head: {
     title: 'test_runexis',
     htmlAttrs: {
-      lang: 'ru'
+      lang: 'ru',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    '@/assets/sass/main.sass'
-  ],
+  css: ['~assets/sass/main.sass'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -35,6 +30,7 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
+    '@nuxtjs/style-resources',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -47,6 +43,9 @@ export default {
   axios: {},
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {},
+
+  styleResources: {
+    sass: ['~assets/sass/utils/*.sass'],
+  },
 }
